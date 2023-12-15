852d3f9d1a36ed9a4971ae3a1dc2a5374c8e19d4
Falkershriek <falkershriek@pm.me>
2023-12-15 18:07:07 +0100

    initialize repository

---
+++ AdventDays/Reusables/SolutionBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.AdventDays.Reusables
+{
+    public abstract class SolutionBase
+    {
+        #region Fields
+        #endregion
+
+        #region Constructors
+        public SolutionBase()
+        {
+            //string filePath = @"D:\Home\Projects\Software\AdventOfCode2023\AdventDays\Day6\TestPuzzleInput.txt";
+            //PuzzleLines = File.ReadLines(filePath).ToList();
+        }
+        #endregion
+
+        #region Enums, Dictionaries and Types
+
+        #endregion
+
+        #region Properties
+        protected abstract List<string> PuzzleLines { get; }
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Parts
+        public abstract long Part1();
+
+        public abstract long Part2();
+        #endregion
+
+        #region Results
+        public long ResultPart1() => Part1();
+
+        public long ResultPart2() => Part2();
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2023.AdventDays.Day1;
+
+namespace AdventOfCode
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Solution solution = new();
+            string solutionString = solution.ResultPart2().ToString();
+            Console.WriteLine($"{solutionString}");
+        }
+    }
+}
+
